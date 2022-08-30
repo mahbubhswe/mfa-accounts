45d5283dbf089eac9ead75caa3fb12175d583f6b
mahbubhswe <mahbubhswe@gmail.com>
2022-08-30 23:54:35 +0600

    add student api update

--- pages/api/addStudent/index.js
+++ pages/api/addStudent/index.js
@@ -7,6 +7,10 @@ handler.use(isAuth);
 handler.post(async (req, res, next) => {
   try {
     await connectMongo();
+    const exit = await Student.findOne({ studentID: req.body.studentID });
+    if (exit) {
+      res.send(`Student ID ${exit.studentID} already exists`);
+    }
     const newStudent = new Student({
       studentID: req.body.studentID,
       name: req.body.name,
