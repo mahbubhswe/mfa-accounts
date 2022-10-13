22a27fa0fde8dfb3dd3ddca1ea71ae2ab3d3232a
mahbubhswe <mahbubhswe@gmail.com>
2022-10-13 15:10:30 +0600

    update s d api 4 b p

--- pages/api/getStudentDetailsForBankPayment/index.js
+++ pages/api/getStudentDetailsForBankPayment/index.js
@@ -1,10 +1,10 @@
 import Student from "../../../models/Student.js";
 import Payment from "../../../models/Payment.js";
 import nextConnect from "next-connect";
+import moment from "moment";
 import connectMongo from "../../../utils/connectMongo.js";
 const handler = nextConnect();
 handler.get(async (req, res, next) => {
-  
   try {
     await connectMongo();
     //get student
@@ -17,11 +17,33 @@ handler.get(async (req, res, next) => {
 
     if (studentInfo) {
       //get payment
-      const payment = await Payment.find(
+      const instalment1 = await Payment.findOne(
         {
           studentId: studentInfo.studentID,
+          instalment: "1st",
         },
-        { studentId: 0, detailsId: 0, createdAt: 0, __v: 0, _id: 0 }
+        { instalment: 1, amount: 1, createdAt: 1, _id: 0 }
+      );
+      const instalment2 = await Payment.findOne(
+        {
+          studentId: studentInfo.studentID,
+          instalment: "2nd",
+        },
+        { instalment: 1, amount: 1, createdAt: 1, _id: 0 }
+      );
+      const instalment3 = await Payment.findOne(
+        {
+          studentId: studentInfo.studentID,
+          instalment: "3rd",
+        },
+        { instalment: 1, amount: 1, createdAt: 1, _id: 0 }
+      );
+      const instalment4 = await Payment.findOne(
+        {
+          studentId: studentInfo.studentID,
+          instalment: "4th",
+        },
+        { instalment: 1, amount: 1, createdAt: 1, _id: 0 }
       );
 
       const studentDetails = {
@@ -29,7 +51,36 @@ handler.get(async (req, res, next) => {
         name: studentInfo.name,
         batch: studentInfo.batch,
         email: studentInfo.email,
-        paymentInfo: payment,
+        paymentInfo: {
+          "1st_instalment": {
+            Status: instalment1 ? "Completed" : "Not Completed",
+            Amount: instalment1 ? instalment1.amount : null,
+            dateOfPayment: instalment1
+              ? moment(instalment1.createdAt).format("MM Do YY")
+              : null,
+          },
+          "2nd_instalment": {
+            Status: instalment2 ? "Completed" : "Not Completed",
+            Amount: instalment2 ? instalment1.amount : null,
+            dateOfPayment: instalment2
+              ? moment(instalment1.createdAt).format("MM Do YY")
+              : null,
+          },
+          "3rd_instalment": {
+            Status: instalment3 ? "Completed" : "Not Completed",
+            Amount: instalment3 ? instalment1.amount : null,
+            dateOfPayment: instalment3
+              ? moment(instalment1.createdAt).format("MM Do YY")
+              : null,
+          },
+          "4th_instalment": {
+            Status: instalment4 ? "Completed" : "Not Completed",
+            Amount: instalment4 ? instalment1.amount : null,
+            dateOfPayment: instalment4
+              ? moment(instalment1.createdAt).format("MM Do YY")
+              : null,
+          },
+        },
       };
       res.send(studentDetails);
     } else {
